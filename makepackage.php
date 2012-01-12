45f2bae57262d911bd2fba824f96f85b867bac8d
Brett Bieber <brett.bieber@gmail.com>
2012-01-12 16:45:15 +0000

    Modify update script to rebuild both SQLite and PDOSQLite databases

--- makepackage.php
+++ makepackage.php
@@ -10,13 +10,14 @@ spl_autoload_register("autoload");
 set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/lib/php');
 
 @unlink('data/spatialdata.sqlite');
-//@unlink('data/spatialdata.pdo.sqlite');
+@unlink('data/spatialdata.pdo.sqlite');
 
+foreach (array('UNL_Geography_SpatialData_SQLiteDriver', 'UNL_Geography_SpatialData_PDOSQLiteDriver') as $driver) {
+    echo "Setting up $driver\n";
+    UNL_Geography_SpatialData_Campus::$driver = new $driver();
 
+    // Now trigger database update by requesting data
+    $campus = new UNL_Geography_SpatialData_Campus();
+    $campus->getGeoCoordinates('501');
+}
 
-$campus = new UNL_Geography_SpatialData_Campus();
-$campus->getGeoCoordinates('501');
-
-
-
-?>
