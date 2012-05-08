f4da6d5f5166822adbd1b2d45e01e04df5e2d8e2
Brett Bieber <brett.bieber@gmail.com>
2012-05-08 16:04:39 -0500

    Use prepared statements for getting geo coordinates for buildings

--- src/UNL/Geography/SpatialData/PDOSQLiteDriver.php
+++ src/UNL/Geography/SpatialData/PDOSQLiteDriver.php
@@ -9,10 +9,41 @@ class UNL_Geography_SpatialData_PDOSQLiteDriver extends UNL_Geography_SpatialDat
         parent::__construct();
     }
 
+    /**
+     * Connect to the database
+     * 
+     * @return PDO
+     */
     protected function __connect()
     {
         $this->db = new PDO('sqlite:'.self::getDataDir().self::$db_file);
         return $this->db;
+    }
+
+    /**
+     * Returns the geographical coordinates for a building.
+     *
+     * @param string $code Building Code for the building you want coordinates of.
+     * @return Associative array of coordinates lat and lon. false on error.
+     */
+    function getGeoCoordinates($code)
+    {
+        $this->_checkDB();
+        static $statement = false;
+
+        if (!$statement) {
+            // Prepare the SQL statement
+            $statement = $this->getDB()->prepare('SELECT lat,lon FROM campus_spatialdata WHERE code = ?');
+        }
+
+        /* @var $statement PDOStatement */
+        if ($statement->execute(array($code))) {
+            while ($coords = $statement->fetch()) {
+                return array('lat'=>$coords['lat'],
+                             'lon'=>$coords['lon']);
+            }
+        }
+        return false;
     }
 
     protected function _getResultRowCount($result)
