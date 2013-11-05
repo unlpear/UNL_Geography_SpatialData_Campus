9d198667214297df142101156ccd014dc78a4cc9
Brett Bieber <brett.bieber@gmail.com>
2013-11-05 12:07:54 -0600

    Adjust order of processing, fallback to SQLite

--- makepackage.php
+++ makepackage.php
@@ -12,7 +12,7 @@ set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/lib
 @unlink('data/spatialdata.sqlite');
 @unlink('data/spatialdata.pdo.sqlite');
 
-foreach (array('UNL_Geography_SpatialData_SQLiteDriver', 'UNL_Geography_SpatialData_PDOSQLiteDriver') as $driver) {
+foreach (array('UNL_Geography_SpatialData_PDOSQLiteDriver', 'UNL_Geography_SpatialData_SQLiteDriver') as $driver) {
     echo "Setting up $driver\n";
     UNL_Geography_SpatialData_Campus::$driver = new $driver();
 
